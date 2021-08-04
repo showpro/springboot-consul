829d085af4476151144b14b52549e929e06db087
zhanzhan <zhanzhan@wahaha.com.cn>
2021-08-04 10:17:20 +0800

    如何在Consul中自定义标签Tag和元数据Metadata（另一种注入方式）

--- server/src/main/java/com/zhan/server/registry/consul/RegistrationCustomizer.java
+++ server/src/main/java/com/zhan/server/registry/consul/RegistrationCustomizer.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.consul.serviceregistry.ConsulRegistration;
 import org.springframework.cloud.consul.serviceregistry.ConsulRegistrationCustomizer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.*;
 
@@ -14,20 +15,28 @@ import java.util.*;
  * @Author zhanzhan
  * @Date 2021/8/4 8:36
  */
+@Component
 public class RegistrationCustomizer implements ConsulRegistrationCustomizer {
 
     /**
      * 加了@Autowired注解了，就是说要放到Spring容器中去，这里跟一般的业务实现不一样。
-     *
+     * <p>
      * 业务实现注入容器我们一般只需要在类上加入@Component、@Controller、@Service等注解就可以了，但是在框架层，扫描包的扫描路径跟业务代码包的
      * 扫描路径是不一样的，是作为一个依赖嵌入在业务代码里面的。所以采用@Configuration注解的方式注入
-     *
      */
-    @Autowired(required = false)
-    private List<MetaCustomizer> metaCustomizers = Collections.EMPTY_LIST;
+//    @Autowired(required = false)
+//    private List<MetaCustomizer> metaCustomizers = Collections.EMPTY_LIST;
+//
+//    @Autowired(required = false)
+//    private List<TagCustomizer> tagCustomizers = Collections.EMPTY_LIST;
 
-    @Autowired(required = false)
-    private List<TagCustomizer> tagCustomizers = Collections.EMPTY_LIST;
+    private List<MetaCustomizer> metaCustomizers;
+    private List<TagCustomizer> tagCustomizers;
+
+    public RegistrationCustomizer(@Autowired(required = false) List<MetaCustomizer> metaCustomizers, @Autowired(required = false) List<TagCustomizer> tagCustomizers) {
+        this.metaCustomizers = metaCustomizers;
+        this.tagCustomizers = tagCustomizers;
+    }
 
     @Override
     public void customize(ConsulRegistration registration) {
--- server/src/main/java/com/zhan/server/registry/consul/configuration/ConsulAutoConfiguration.java
+++ server/src/main/java/com/zhan/server/registry/consul/configuration/ConsulAutoConfiguration.java
@@ -6,15 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @Description
+ * @Description 如果不采用这种方式注入，META-INF中的 EnableAutoConfiguration 配置也要注释掉
  * @Author zhanzhan
  * @Date 2021/8/4 9:07
  */
-@Configuration
-public class ConsulAutoConfiguration {
-
-    @Bean
-    public ConsulRegistrationCustomizer consulRegistrationCustomizer() {
-        return new RegistrationCustomizer();
-    }
-}
+//@Configuration
+//public class ConsulAutoConfiguration {
+//
+//    @Bean
+//    public ConsulRegistrationCustomizer consulRegistrationCustomizer() {
+//        return new RegistrationCustomizer();
+//    }
+//}
